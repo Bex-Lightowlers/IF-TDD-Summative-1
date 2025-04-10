133da7297b622aa0443749611e4dacc9f562b515
Bex-Lightowlers <bex_holtby@hotmail.co.uk>
2025-04-10 19:09:08 +0100

    Smoke test works

--- test_leap_years.py
+++ test_leap_years.py
@@ -4,15 +4,12 @@ def is_leap_year(year):
     return (year % 4 ==0 and year % 100 !=0) or (year % 400 ==0)
 
 class TestLeapYear(unittest.TestCase):
+
     def test_true_cases(self):
         self.assertTrue(is_leap_year(2000))
-        self.assertTrue(is_leap_year(2024))
-        self.assertTrue(is_leap_year(2400))
+ 
 
-    def test_false_cases(self):
-        self.assertFalse(is_leap_year(1900))
-        self.assertFalse(is_leap_year(2023))
-        self.assertFalse(is_leap_year(1800))
+   
 
 if __name__ == '__main__':
     unittest.main()
