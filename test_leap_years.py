12dfc0f556c73a53ecfc6c13bea0fec4770ee077
Bex-Lightowlers <bex_holtby@hotmail.co.uk>
2025-04-10 19:05:00 +0100

    Pass Smoke testing

--- test_leap_years.py
+++ test_leap_years.py
@@ -3,11 +3,20 @@ import unittest
 def is_leap_year(year):
     return (year % 4 ==0 and year % 100 !=0) or (year % 400 ==0)
 
-year = int(input("Enter a year: "))
-if is_leap_year(year):
-    print(f"{year} is a leap year")
-else:
-    print(f"{year} is not a leap year")
+class TestLeapYear(unittest.TestCase):
+    def test_true_cases(self):
+        self.assertTrue(is_leap_year(2000))
+        self.assertTrue(is_leap_year(2024))
+        self.assertTrue(is_leap_year(2400))
+
+    def test_false_cases(self):
+        self.assertFalse(is_leap_year(1900))
+        self.assertFalse(is_leap_year(2023))
+        self.assertFalse(is_leap_year(1800))
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 
 
