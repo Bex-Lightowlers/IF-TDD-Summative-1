32d22b54f59ff65efd5248890b8fb06528afc4d3
Bex-Lightowlers <bex_holtby@hotmail.co.uk>
2025-04-10 20:14:10 +0100

    Passed test

--- test_leap_years.py
+++ test_leap_years.py
@@ -8,7 +8,7 @@ class TestLeapYear(unittest.TestCase):
 
     def test_true_cases(self):
         self.assertTrue(is_leap_year(2000))
-        self.assertTrue(is_leap_year(2001))
+        self.assertTrue(is_leap_year(2004))
 
     def test_false_cases(self):
         self.assertFalse(is_leap_year(2025))
