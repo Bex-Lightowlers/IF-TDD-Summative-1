b5975bf1e8397d30a8fcc127b57dd388a8f97cec
Bex-Lightowlers <bex_holtby@hotmail.co.uk>
2025-04-10 19:16:58 +0100

    New year

--- test_leap_years.py
+++ test_leap_years.py
@@ -1,4 +1,5 @@
 import unittest
+from year import is_leap_year
 
 def is_leap_year(year):
     return (year % 4 ==0 and year % 100 !=0) or (year % 400 ==0)
@@ -7,10 +8,10 @@ class TestLeapYear(unittest.TestCase):
 
     def test_true_cases(self):
         self.assertTrue(is_leap_year(2000))
+
+    def test_false_cases(self):
+        self.assertFalse(is_leap_year(2001))
  
-
-   
-
 if __name__ == '__main__':
     unittest.main()
 
