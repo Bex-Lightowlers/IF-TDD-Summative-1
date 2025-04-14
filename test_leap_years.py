738e35751ac27fd75273525ec4ef5ba11b5b0ceb
Bex-Lightowlers <bex_holtby@hotmail.co.uk>
2025-04-14 20:02:54 +0100

    pass

--- test_leap_years.py
+++ test_leap_years.py
@@ -3,14 +3,14 @@ from year import is_leap_year
 
 class TestLeapYear(unittest.TestCase):
     def test_true_cases(self):
-        self.assertTrue(is_leap_year(2000))
-        self.assertTrue(is_leap_year(2004))
-        self.assertTrue(is_leap_year(2024))
+        self.assertTrue(is_leap_year(2000)) #is a leap year 
+        self.assertTrue(is_leap_year(2004)) #is a leap year 
+        self.assertTrue(is_leap_year(2024)) #is a leap year 
 
     def test_false_cases(self):
-        self.assertFalse(is_leap_year(1990))
-        self.assertFalse(is_leap_year(2001))
-        self.assertFalse(is_leap_year(2025))
+        self.assertFalse(is_leap_year(1990)) #is not a leap year 
+        self.assertFalse(is_leap_year(2001)) #is not a leap year 
+        self.assertFalse(is_leap_year(2025)) #is not a leap year 
  
 if __name__ == '__main__':
     unittest.main()
--- year.py
+++ year.py
@@ -1,12 +1,2 @@
 def is_leap_year(year):
     return (year % 4 ==0 and year % 100 !=0) or (year % 400 ==0)
-user_input = input("Enter Year ")
-
-try:
-    year=int(user_input)
-    if is_leap_year (year):
-        print(f"{year} is a leap year")
-    else:
-        print(f"{year} is a not leap year")
-except ValueError:
-    print("Please enter a valid number.")
